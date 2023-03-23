82f097936b133a5f5647cf3ae1a7bb393c2b93f5
hanohirani <hanohirani@gmail.com>
2023-03-23 17:40:55 +0300

    uploading butoon

--- asset/js/UploadFolder.js
+++ asset/js/UploadFolder.js
@@ -1,22 +1,21 @@
 function UploadFolder (){
    
-        let folder = document.getElementById("formFile").files[0];
-        let folderSize = document.getElementById("formFile").files[0].size;
+        let folder = document.getElementById("formFile");
+        let folderSize = document.getElementById("formFile").size;
         let time = new Date().getTime();
-      
+        var formData = new FormData()
+        formData.append('folder', folder);
+        formData.append('size', folderSize);
+        formData.append('dates', time);
         $.ajax({
             url: "../../controller/file-manager.php",
             type: "POST",
-            data: {
-                upload: "upload",
-                folder: folder,
-                size: folderSize,
-                date: time
-            },
-            contentType: false,
+            data: formData,
             processData: false,
+            contentType: false,
             success: function (data,status) {
                 console.log(status);
+                console.log(data);
             }
            
         });
--- config/config.php
+++ config/config.php
@@ -1,19 +1,11 @@
 <?php
 
-define("Host", "localhost");
-define("dbname", "storage");
-define("user", "root");
-define("pass", "");
-$db = null;
-
-     try{
-        $db = new PDO("mysql:host=". Host .";dbname=". dbname .";charset=utf8", user, pass,
-        [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]);
-        return $db;
-   }
-   catch (PDOException $e) {
-      echo "Error!: " . $e->getMessage() . "<br/>";
-  }
+$host =  "localhost"; // define("host", "localhost");
+$dbname ="storage"; // define("dbnm", "example_system_db");
+$user = "root"; // define("user", "root");
+$pass = ""; // define("pass", "password");
 
 
-// $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$user, $pass);
+   $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass,
+   [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]);
+ 
